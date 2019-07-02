08438c66333a2276caab74df1fab4bcfa34e3739
Odysseus <arwaterman@gmail.com>
2019-07-01 17:53:31 -0700

    added in custom modal

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -69,7 +69,7 @@ function gameLost() {
     document.getElementById("random-number").textContent = randomNumber;
 
     // alert the user
-    alert("You lost!");
+    gameOver("You lost!");
 
     console.log("You lost!");
     console.log("------------------------------------");
@@ -89,7 +89,7 @@ function gameWon() {
     document.getElementById("random-number").textContent = randomNumber;
 
     // alert the user
-    alert("You won!");
+    gameOver("You won!");
 
     console.log("You won!");
     console.log("------------------------------------");
@@ -108,7 +108,33 @@ function gameReset() {
         }
 }
 
+function gameOver(msg) {
+    const gameOver = document.getElementById('game-over-msg');
+    gameOver.textContent = msg;
+
+    const modal = document.getElementById("myModal");
+    modal.style.display = "block";
+}
+
 // Game Start
 document.getElementById("random-number").textContent = randomNumber;
 
 render();
+
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
